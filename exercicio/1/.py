dab62d6611dd14d612cf05d6bc718792382c7e7a
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-15 16:36:21 -0300

    feat: tipagem e operadoes matematicos

---
+++ exercicio/1/.py
@@ -0,0 +1,21 @@
+
+import os 
+os.system("cls")
+
+
+
+
+numero = float(input("Digite o número 1: "))
+numero2 = float(input("Digite o número 2: "))
+
+print(f"Número 1: {numero}")
+print(f"Número 2: {numero2}")
+
+soma = numero + numero2
+print(f"A soma é: {soma}")
+subtracao = numero - numero2
+print(f"A subtrção é: {subtracao}")
+multiplicacao = numero * numero2
+print(f"A multiplicção é: {multiplicacao}")
+divisao = numero / numero2
+print(f"A divisão é: {divisao}")
