59ceb56fb44e956055ffa149e186eb2393ca8318
Lukask40 <lukasgaby2016@gmail.com>
2025-08-22 16:53:34 -0300

    Add files via upload

---
+++ 1 condicionais_simples.py
@@ -0,0 +1,13 @@
+# Condicinais simples.
+
+import os 
+os.system("cls")
+
+idade = 20 
+
+if idade < 18:
+    print("Menor de idade.")
+
+print("Fim.")
+       
+
---
+++ atv.py
@@ -0,0 +1,11 @@
+import os 
+os.system("cls")
+
+numero = int(input("Digite o número: "))
+
+if numero > 10:
+    print("É MAIOR QUE 10.")
+elif numero == 10:
+    print ("O NÚMERO É IGUAL A 10.")
+else:
+    print("O NÚMERO É MENOR QUE 10")       
