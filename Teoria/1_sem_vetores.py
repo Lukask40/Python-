9634e84612364c8ecc875d8ebb4e1f054e397eb1
Lukask40 <lukasgaby2016@gmail.com>
2025-10-01 17:12:55 -0300

    Add files via upload

---
+++ Teoria/1_sem_vetores.py
@@ -0,0 +1,16 @@
+import os
+os.system("cls")
+
+soma = 0
+
+for i in range(3):
+    nota = int(input(f"Digite a {i+1}ª nota: "))
+    soma += nota
+
+
+
+print(f"Notas: {nota}")
+print(f"Soma: {soma}")
+
+
+print("FIM")
